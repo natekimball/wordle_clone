5fc061bd2a1c7c7fb483b3311be2a5a23b204dee
natekimball <natekimball1@gmail.com>
2022-11-20 21:38:16 -0500

    updated dictionary

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -24,15 +24,3 @@ impl Dictionary {
         guess.chars().any(|x| !x.is_ascii_alphabetic()) || !self.words.contains(guess)
     }
 }
-
-// pub async fn get_dict_set() -> Box<HashSet<String>> {
-//     let dictionary = HashSet::new();
-//     let fstring = fs::read_to_string("src/resources/dictionary.txt").unwrap();
-//     let lines = fstring.lines();
-//     lines.par_bridge().for_each(|line| { dictionary.clone().insert(line.to_string()); });
-//     Box::new(dictionary)
-// }
-
-// pub fn invalid_guess(guess: &String) -> bool {
-//     guess.chars().any(|x| !x.is_ascii_alphabetic()) || std::fs::read_to_string("src/resources/dictionary.txt").unwrap().lines().all(|line| *guess != line.to_string())
-// }
